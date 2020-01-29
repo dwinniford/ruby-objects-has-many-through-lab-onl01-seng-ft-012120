4b3868cb369a87b273b84ef9f0531631cac3db36
Dave Winniford <dwinniford@gmail.com>
2020-01-29 20:37:23 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,4 +12,18 @@ class Patient
     @@all 
   end 
   
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end 
+  
+  def appointments
+    Appointment.all.select { |a| a.patient == self }
+  end 
+  
+  def doctors 
+    appointments.map { |a| a.doctor }
+  end 
+  
+end 
+  
   
