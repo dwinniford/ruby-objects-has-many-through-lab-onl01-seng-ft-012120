1debdf4faf5181f24eee609049fadd138d69473b
Dave Winniford <dwinniford@gmail.com>
2020-01-29 20:05:30 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -3,4 +3,21 @@ class Genre
   
   @@all = []
   
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
   
+  def all 
+    @@all 
+  end 
+  
+  def songs 
+    Songs.all.select { |s| s.genre = self }
+  end 
+  
+  def artists
+    songs.map { |s| s.artist }
+  end 
+  
+end 
